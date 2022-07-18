fb064044724539808eedb5a6603a657ab8f98f4b
mutedspeaker <60806481+mutedspeaker@users.noreply.github.com>
2022-07-18 22:21:57 +0530

    Update bb.py

--- bb.py
+++ bb.py
@@ -2,22 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import openpyxl
 import datetime
-
-# https://www.amazon.in/s?k= biscuit &page=2
-
-site = "https://www.amazon.in/s?k="
-
-# to match
-keyword = "biscuit"
-excel = openpyxl.Workbook()
-sheet = excel.active
-sheet.title = 'Britannia'
-sheet.append(
-    ["Platform", "Date", "Category", "Brand name", "Product Name", "Rating", "Reviews", "Discount", "Size", "Price/g",
-     "Keyword", "Actual price", "Offer price", "Delivery Date", "Page Number", "Item Number"])
+import pandas as pd
+import re as rrr
 
 
-def soupExtract(site, keyword, brand=0, page=1):
+# an amazon tailored soup extracting function
+def soupExtract(site, keyword, page, brand=0):
     if brand != 0:
         sheet.title = brand
     url = site + keyword + "&page=" + str(page)
@@ -29,8 +19,9 @@ def soupExtract(site, keyword, brand=0, page=1):
             soup = BeautifulSoup(req.text, "html.parser")
             break
 
-        except Exception as e:
-            print("Retrying...", end=" ")
+        # the exception is due, to high traffic in Amazon Website, it is quite possible to not get our html request in the first time
+        except Exception:
+            print("r...", end=" ")
     a = soup.find('span', class_='rush-component s-latency-cf-section').find_all('span')
     name = soup.find('span', class_='rush-component s-latency-cf-section').find_all('span',
                                                                                     class_='a-size-base-plus a-color-base a-text-normal')
@@ -64,6 +55,11 @@ def soupExtract(site, keyword, brand=0, page=1):
             re = int(reviews[i].text.replace(',', ''))
             # discount
             si = name[i].text.split()[-1]
+            # try:
+            #     si = rrr.search("([0-9]{1,10})\.{0,1}[0-9]*\s{0,3}(g)", name[i].text).group()
+            # except:
+            #     si = name[i].text.split()[-1]
+            # si = rrr.find("[0-9]*/.{0,1}[0-9]*/s{0,3}(g)", name[i].text)
             wpg = weightPerGram[i].text
             k = keyword
             b = float(beforeprice[i].text.split('₹')[1].replace(',', ''))
@@ -73,48 +69,104 @@ def soupExtract(site, keyword, brand=0, page=1):
             di = di / b
             di = di * 100
             di = round(di, 2)
+            # if di > 0 :
+            #     di = str(di) + "%"
+            # else:
+            #     di = 0
+            #     b = 0
             di = str(di) + "%"
+            wbst = "aafaf"
+            # keyword
+            if k.lower() not in name[i].text.lower():
+                continue
+            if (brand != 0) & (bn != brand):
+                continue
+            rdt.append([pl, dt, ct, bn, n, r, re, di, si, wpg, k, b, p, de, page, i + 1, wbst])
+            sheet.append([pl, dt, ct, bn, n, r, re, di, si, wpg, k, b, p, de, page, i + 1, wbst])
+
         except:
             pass
-        # keyword
-        if k.lower() not in name[i].text.lower():
-            continue
-        if (brand != 0) & (bn != brand):
-            continue
-        rdt.append([pl, dt, ct, bn, n, r, re, di, si, wpg, k, b, p, de, page, i + 1])
-        sheet.append([pl, dt, ct, bn, n, r, re, di, si, wpg, k, b, p, de, page, i + 1])
+
 
     pager = soup.find('span', {'class': 's-pagination-strip'})
-    print(pager.text[-5])
-    totalpages = int(pager.text[-5])
+
+    # store the total number of pages in this variable and return it at the end
+    totalPages = int(pager.text[-5])
+
     #     print(pager.find('span', class_ = 's-pagination-item s-pagination-disabled'))
     #     if pager.find('span', class_ = 's-pagination-item s-pagination-disabled'):
     #         return 1
     #     else:
     #         return 0
-    return totalpages
 
-#     totalPages = soup.find('span', 'aria-disabled'=='true').text
-#     print(totalPages)
-#     print(rdt)
+    #     totalPages = soup.find('span', 'aria-disabled'=='true').text
+    #     print(totalPages)
+    #     print(rdt)
 
-keywords = ['biscuit']
-# print('Number of keywords ')
+    return totalPages
 
-c = input()
-a = []
-for i in range(int(c)):
-    a .append(input())
-a = keywords
-# totalpages = soupExtract(site,'cream biscuit')
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# reading excel file
+# hardcoded path
+data = pd.read_excel(r'C:\Users\Ojas Tewari\Desktop\input.xlsx')
+
+data = pd.read_excel(r'input.xlsx')
+
+# # if the user wants to give the path of the excel file
+# print("Do u want to give a separate path? ")
+# ans = input()
+# if ans.lower() == 'y' or ans.lower() == 'yes':
+#     path = input()
+#     path = path[1:-1]
+#     data = pd.read_excel(path)
+
+print("Enter column name: \n")
+keywords = input()
+df = pd.DataFrame(data, columns=[keywords])
+# print(df)
+# print(df.head(2))
+# print(df.shape)
+# print(df.describe())
+
+# Converting data frame taken from the above keyword to a numpy array
+arr = df.to_numpy()
+keywords = []
+
+# Numpy arrays are 2d so making it 1d
+for i in arr:
+    if type(i[0]) == str:
+        keywords.append(i[0])
+
+# A typical amazon website broken in 3 parts:
+# https://www.amazon.in/s?k= biscuit &page=2
+
+site = "https://www.amazon.in/s?k="
+
+# Open excel sheet
+excel = openpyxl.Workbook()
+sheet = excel.active
+
+# Add the first row, with the following requirements
+sheet.append(
+    ["Platform", "Date", "Category", "Brand name", "Product Name", "Rating", "Reviews", "Discount", "Size", "Price/g",
+     "Keyword", "Actual price", "Offer price", "Delivery Date", "Page Number", "Item Number", "Website"])
+# ---------------------------------------------------------------------------------------------------------------------
+
+# totalPages = soupExtract(site,'cream biscuit')
+# for i in range(2, int(total_pages) + 1):
+
 for keyword in keywords:
-    totalpages = soupExtract(site, keyword)
-#     for i in range(2, int(totalpages) + 1):
+
+    total_pages = soupExtract(site, keyword, 1)         # brand
     i = 1
-    while i!=totalpages:
+    while i != total_pages:
         i = i+1
         try:
-            soupExtract(site, keyword, 'Cadbury',i)
+            soupExtract(site, keyword, i)               # brand
         except:
             pass
+
+# saving the excel file
 excel.save("Data.xlsx")
